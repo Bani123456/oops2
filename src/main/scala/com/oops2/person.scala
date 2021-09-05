1e88a35ed50fb09e423bf788e97a9b9623b88398
Bani123456 <himani.chauhan@knoldus.com>
2021-09-05 16:50:46 +0530

    assignment of oops

---
+++ src/main/scala/com/oops2/person.scala
@@ -0,0 +1,33 @@
+package com.oops2
+class Person(val name : String, val age: Int) extends Ordered[Person] {
+  override def compare(that: Person): Int = {
+    if(this.name == that.name)
+      this.age - that.age
+    else
+      this.name.length - that.name.length
+  }
+}
+
+
+object MainObject{
+
+  def main(args:Array[String]): Unit ={
+
+    val personOne= new Person(name= "John", age=29)
+    val personTwo= new Person(name= "John", age=30)
+    val personThree= new Person(name= "John1", age=29)
+    val personFour= new Person(name= "John", age=30)
+
+    def objectComparison(person1: Person, person2: Person): Unit = {
+      if(person1.compare(person2) > 0)
+        println(false)
+      else
+        println(true)
+    }
+    objectComparison(personOne,personTwo)
+    objectComparison(personThree,personFour)
+
+  }
+
+}
+
